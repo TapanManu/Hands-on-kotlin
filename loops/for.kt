9c2a145ce539e40a09ecfa162d90af34db354639
Tapan Manu <48797926+TapanManu@users.noreply.github.com>
2020-06-18 16:39:08 +0530

    for loop

---
+++ loops/for.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("first loop")
+for (i:Int in 1..3) {
+    println(i)
+}
+println("second loop")
+for (i:Int in 6 downTo 0 step 2) {
+    println(i)
+}
+println("third loop")
+for(i:Int in 0..6 step 3){
+    println(i)
+}
+
+}
