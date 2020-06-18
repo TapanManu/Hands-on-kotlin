8266ee00b8c0d97ab377206b9432fc34e4428d07
Tapan Manu <48797926+TapanManu@users.noreply.github.com>
2020-06-18 16:49:06 +0530

    return from when

---
+++ condition/return_from_when.kt
@@ -0,0 +1,11 @@
+fun main(){
+val count=43
+val answerString = when {
+    count == 42 -> "I have the answer."
+    count > 35 -> "The answer is close."
+    else -> "The answer eludes me."
+}
+
+println(answerString)
+
+}
